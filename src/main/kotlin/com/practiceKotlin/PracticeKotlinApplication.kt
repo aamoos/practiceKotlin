96340ababd4ee05f6abdb91d6fff1313a8e44c83
김재성 <aamoos@openmate.co.kr>
2024-03-29 13:25:47 +0900

    Initial commit

---
+++ src/main/kotlin/com/practiceKotlin/PracticeKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.practiceKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticeKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/practiceKotlin/PracticeKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.practiceKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
