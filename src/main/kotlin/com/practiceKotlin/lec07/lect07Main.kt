360446d596bc8ada073d19945b5725635fc27e2d
김재성 <aamoos@openmate.co.kr>
2024-04-08 10:28:08 +0900

    코틀린에서 함수를 다루는 방법

---
+++ src/main/kotlin/com/practiceKotlin/lec07/lect07Main.kt
@@ -0,0 +1,38 @@
+package com.practiceKotlin.lec07
+
+fun main(){
+    repeat("Hello World", useNewLine = false)
+    printNameAndGender(name = "최태현", gender = "남자")
+    //Lec07Main.repeat(str = "A")
+    printAll("A", "B", "C")
+
+    val array = arrayOf("A", "B", "C")
+    printAll(*array)
+}
+
+fun max(a: Int, b: Int) = if(a > b) a else b
+
+fun repeat(
+    str: String
+    , num: Int = 3
+    , useNewLine: Boolean = true
+){
+    for (i in 1..num){
+        if(useNewLine){
+            println(str)
+        }else{
+            print(str)
+        }
+    }
+}
+
+fun printNameAndGender(name: String, gender: String){
+    println(name)
+    println(gender)
+}
+
+fun printAll(vararg strings: String){
+    for (str in strings){
+        println(str)
+    }
+}
