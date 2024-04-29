76120b06f22f0a3828e132f67bddc85a04749ce7
kimjaesung <aamoos@naver.com>
2024-04-29 23:01:26 +0900

    20강. 코틀린의 scope function

---
+++ src/main/kotlin/com/practiceKotlin/lec20/Lec20Main.kt
@@ -0,0 +1,11 @@
+package com.practiceKotlin.lec20
+
+fun main(){
+    mutableListOf("one", "two", "three")
+        .also { println("The list elements before adding new one: $it") }
+        .add("four")
+
+    val numbers = mutableListOf("one", "two", "three")
+    println("The list elements before adding new one: $numbers")
+    numbers.add("four")
+}
